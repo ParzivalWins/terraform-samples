30b6a70ec11874b5e2183233ac1be281cac6c3eb
Ben Morris <agentbond007@users.noreply.github.com>
2017-05-19 16:33:59 -0700

    Update main.tf
    Added AWS features

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
-
-resource "google_compute_instance" "server" {
-      name  = "server"
-      machine_type = "g1-micro"
-      zone         = "us-central1-a"
-
-      disk  {
-         image =  "ubuntu-1404-trusty-v20160114e"
-      }
+provider "aws" {
+  access_key = "insert"
+  secret_key = "insert"
+  region     = "us-east-1"
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0b33d91d"
+  instance_type = "t2.micro"
+}
+
+
+
+#resource "google_compute_instance" "server" {
+#     name  = "server"
+#      machine_type = "g1-micro"
+#      zone         = "us-central1-a"
+#
+#      disk  {
+#         image =  "ubuntu-1404-trusty-v20160114e"
+#      }
+#}
